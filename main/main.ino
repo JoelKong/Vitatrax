18c86ba08daa740c069e3c41cb9691fb93173ffd
Joel Kong <joelkong2001@gmail.com>
2023-10-25 12:55:52 +0800

    fix: restructure code

--- main/main.ino
+++ main/main.ino
@@ -11,6 +11,8 @@ const unsigned int* sprites[3] = {playerFaceSouthBMP, playerWalkSouthLeftBMP, pl
 int animationCounter = 0;
 bool isAnimating = true;
 bool firstSpriteShown = false;
+const unsigned long DEBOUNCE_DELAY = 200; // in milliseconds
+unsigned long lastButtonPressTime = 0;
 
 void setup() {
   initializeDisplay();
@@ -25,28 +27,28 @@ void loop() {
   handleButtonPresses();
 }
 
-
-
-// Handle navigation between the 3 screens
 void handleButtonPresses() {
-  if (isAnimating && (display.getButtons() & TSButtonLowerRight)) {
-    transitionToMenuScreen();
-  } else if (!isAnimating && (display.getButtons() & TSButtonUpperLeft)) {
-    transitionToAnimationScreen();
+  unsigned long currentTime = millis();
+  if ((currentTime - lastButtonPressTime) > DEBOUNCE_DELAY) {
+    if (isAnimating && (display.getButtons() & TSButtonLowerRight)) {
+      transitionToMenuScreen();
+      lastButtonPressTime = currentTime;
+    } else if (!isAnimating && (display.getButtons() & TSButtonUpperLeft)) {
+      transitionToAnimationScreen();
+      lastButtonPressTime = currentTime;
+    }
   }
 }
 
 void transitionToMenuScreen() {
   isAnimating = false;
   drawMenu();
-  delay(200);
 }
 
 void transitionToAnimationScreen() {
   isAnimating = true;
   firstSpriteShown = false;
   drawInitialScreen();
-  delay(200);
 }
 
 
@@ -64,6 +66,8 @@ void initializeDisplay() {
 
 void drawInitialScreen() {
   display.drawRect(0, 0, 96, 64, TSRectangleFilled, TS_8b_White);
+  drawRightArrow(88, 54); // Bottom-right arrow
+  drawTextBesideArrow("Go", 70, 53);
 }
 
 void displayAnimationScreen() {
@@ -90,22 +94,8 @@ void drawAnimationNavigation() {
   const int arrowBaseX = 88;
   const int arrowBaseY = 54;
 
-  drawArrow(arrowBaseX, arrowBaseY);
+  drawRightArrow(arrowBaseX, arrowBaseY);
   drawTextBesideArrow("Go", arrowBaseX - 18, 53);
-}
-
-void drawArrow(int baseX, int baseY) {
-  display.drawLine(baseX, baseY, baseX + 6, baseY + 4, TS_8b_Green);
-  display.drawLine(baseX, baseY + 8, baseX + 6, baseY + 4, TS_8b_Green);
-  for (int i = 0; i < 6; i++) {
-    display.drawLine(baseX + i, baseY + i, baseX + i, baseY + 8 - i, TS_8b_Green);
-  }
-}
-
-void drawTextBesideArrow(const char* text, int x, int y) {
-  display.fontColor(TS_8b_Green, TS_8b_Black);
-  display.setCursor(x, y);
-  display.print(text);
 }
 
 void drawBMPImage(int x, int y, const unsigned int* image, int width, int height) {
@@ -119,6 +109,30 @@ void drawBMPImage(int x, int y, const unsigned int* image, int width, int height
   }
 }
 
+void drawRightArrow(int baseX, int baseY) {
+  display.drawLine(baseX, baseY, baseX + 6, baseY + 4, TS_8b_Green);
+  display.drawLine(baseX, baseY + 8, baseX + 6, baseY + 4, TS_8b_Green);
+  for (int i = 0; i < 6; i++) {
+    display.drawLine(baseX + i, baseY + i, baseX + i, baseY + 8 - i, TS_8b_Green);
+  }
+}
+
+void drawLeftArrow(int baseX, int baseY) {
+    // Adjusted to make the arrow point to the left
+    display.drawLine(baseX + 5, baseY, baseX, baseY + 4, TS_8b_Green);
+    display.drawLine(baseX + 5, baseY + 8, baseX, baseY + 4, TS_8b_Green);
+    for (int i = 0; i < 6; i++) {
+        display.drawLine(baseX + 5 - i, baseY + i, baseX + 5 - i, baseY + 8 - i, TS_8b_Green);
+    }
+}
+
+void drawTextBesideArrow(const char* text, int x, int y) {
+  display.fontColor(TS_8b_Green, TS_8b_Black);
+  display.setCursor(x, y);
+  display.print(text);
+}
+
+
 
 
 
@@ -128,10 +142,10 @@ void drawMenu() {
   display.clearScreen();
 
   // This can be expanded for more menu items later on
-  drawArrow(4, 4); // Top-left arrow
+  drawLeftArrow(4, 4); // Top-left arrow pointing left
   drawTextBesideArrow("Log", 14, 3);
 
-  drawArrow(88, 54); // Bottom-right arrow
+  drawRightArrow(88, 54); // Bottom-right arrow
   drawTextBesideArrow("Go", 70, 53);
 }
 
